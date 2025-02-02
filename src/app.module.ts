3de2f2bd148fef9e740ce9f0b34bfd2b373de394
MorganeLM <lets.morganise.it@gmail.com>
2025-02-02 22:50:13 +0100

    chore: add TypeORM and PostgreSQL configuration to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { accommodationsModule } from './accommodations/accommodations.module';
+import { AccommodationsModule } from './accommodations/accommodations.module';
+import { Accommodation } from './accommodations/accommodation.entity';
 
 @Module({
-  imports: [accommodationsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'echoaway',
+      password: 'echoaway',
+      database: 'echoaway',
+      entities: [Accommodation],
+      synchronize: true,
+    }),
+    AccommodationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
