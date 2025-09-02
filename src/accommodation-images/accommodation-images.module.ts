c0519137b4c08b0981ae35dbfd8b37df86b60855
MorganeLM <lets.morganise.it@gmail.com>
2025-09-02 20:47:18 +0200

    feat: setup TypeORM module structure and architecture - Create users module structure - Create accommodations module structure - Create favorites module structure - Create accommodation-images module structure

---
+++ src/accommodation-images/accommodation-images.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccommodationImage } from './accommodation-image.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AccommodationImage])],
+  exports: [TypeOrmModule],
+})
+export class AccommodationImagesModule {}
